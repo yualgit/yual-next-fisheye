6c581a1a1d50e39e99f4a93730c90cd95ff730c5
Vasyl Hrebeniuk <vasiliyhrebenuyk@MacBook-Pro-Vasiliy-3.local>
2025-10-02 12:30:43 +0300

    misc: change text for welcome

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import FisheyeTextScene from "./components/FisheyeTextScene";
 
 export default function Home() {
   const sampleText =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    "Hey Tom! Check this out – we validated the concept for your project and wanted to share it with you. We couldn't think of a better way than making this little demo for you. Always dedicated – the weirdos from yual";
 
   return (
     <div className="fixed inset-0 w-full h-full overflow-hidden">
